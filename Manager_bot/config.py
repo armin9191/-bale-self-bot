811dea88901fcb4283015e41fefcace8551eea88
armin9191 <armin.golestani1402@gmail.com>
2026-09-22 18:27:28 +0330

    Create config.py

---
+++ Manager_bot/config.py
@@ -0,0 +1,7 @@
+# ==============================
+# Group Manager Bot - Config
+# ==============================
+
+BOT_TOKEN = "1200170625:QA5mqJZopacY2FcC5lTimYyQpyRviFvJ6Lg"
+
+API_URL = f"https://tapi.bale.ai/bot{BOT_TOKEN}"
