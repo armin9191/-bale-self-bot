bc901009ea26f756b81cded8a2a4f036de10ae63
armin9191 <armin.golestani1402@gmail.com>
2026-09-22 18:56:35 +0330

    Update config.py

--- Manager_bot/config.py
+++ Manager_bot/config.py
@@ -2,6 +2,20 @@
 # Group Manager Bot - Config
 # ==============================
 
-BOT_TOKEN = "1200170625:QA5mqJZopacY2FcC5lTimYyQpyRviFvJ6Lg"
+import os
+
+
+# ==============================
+# Bot
+# ==============================
+
+BOT_TOKEN = os.getenv("1200170625:QA5mqJZopacY2FcC5lTimYyQpyRviFvJ6Lg")
 
 API_URL = f"https://tapi.bale.ai/bot{BOT_TOKEN}"
+
+
+# ==============================
+# Database
+# ==============================
+
+DATABASE_NAME = "/data/bot.db"
